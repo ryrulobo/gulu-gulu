0a6582bf410d5c9e0dcc441576daa042504bda5c
ryrulobo <ryru.lobo@gmail.com>
2022-10-28 16:56:01 +0700

    fix server route

--- server/app.js
+++ server/app.js
@@ -5,13 +5,13 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./middlewares/errorHandler");
-const route = require("./routes");
+const router = require("./routes");
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use("/", route);
+app.use(router);
 app.use(errorHandler);
 
 module.exports = app;
